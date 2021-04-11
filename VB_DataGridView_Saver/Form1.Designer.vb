f1bfa7ae35d901b1ed75d21090643890f3ee2abe
SydneyPrice15 <SydneyPrice15@users.noreply.github.com>
2021-04-11 22:57:43 +0800

    Initial commit

---
+++ VB_DataGridView_Saver/Form1.Designer.vb
@@ -0,0 +1,458 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtSid = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnAddnew = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnreset = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtFn = New System.Windows.Forms.TextBox()
+        Me.txtSn = New System.Windows.Forms.TextBox()
+        Me.txtdob = New System.Windows.Forms.TextBox()
+        Me.txtadress = New System.Windows.Forms.TextBox()
+        Me.txtmobile = New System.Windows.Forms.TextBox()
+        Me.txtemail = New System.Windows.Forms.TextBox()
+        Me.btndel = New System.Windows.Forms.Button()
+        Me.btnext = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FeildToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.cmbgen = New System.Windows.Forms.ComboBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtSid
+        '
+        Me.txtSid.Location = New System.Drawing.Point(145, 44)
+        Me.txtSid.Name = "txtSid"
+        Me.txtSid.Size = New System.Drawing.Size(100, 20)
+        Me.txtSid.TabIndex = 0
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8})
+        Me.DataGridView1.Location = New System.Drawing.Point(49, 315)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(848, 257)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(67, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Student ID"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.btnext)
+        Me.Panel1.Controls.Add(Me.btndel)
+        Me.Panel1.Controls.Add(Me.btnreset)
+        Me.Panel1.Controls.Add(Me.btnSave)
+        Me.Panel1.Controls.Add(Me.btnPrint)
+        Me.Panel1.Controls.Add(Me.btnAddnew)
+        Me.Panel1.Location = New System.Drawing.Point(951, 44)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(259, 506)
+        Me.Panel1.TabIndex = 3
+        '
+        'btnAddnew
+        '
+        Me.btnAddnew.Location = New System.Drawing.Point(91, 103)
+        Me.btnAddnew.Name = "btnAddnew"
+        Me.btnAddnew.Size = New System.Drawing.Size(75, 23)
+        Me.btnAddnew.TabIndex = 0
+        Me.btnAddnew.Text = "add new"
+        Me.btnAddnew.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(91, 157)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrint.TabIndex = 0
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(91, 198)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 0
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnreset
+        '
+        Me.btnreset.Location = New System.Drawing.Point(91, 249)
+        Me.btnreset.Name = "btnreset"
+        Me.btnreset.Size = New System.Drawing.Size(75, 23)
+        Me.btnreset.TabIndex = 0
+        Me.btnreset.Text = "Reset"
+        Me.btnreset.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(454, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "address"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(76, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Fristname"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(76, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Surname"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(454, 47)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 13)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Gender"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(454, 179)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 13)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "mobile"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(70, 264)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Date of Birth"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(454, 258)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(34, 13)
+        Me.Label8.TabIndex = 2
+        Me.Label8.Text = " email"
+        '
+        'txtFn
+        '
+        Me.txtFn.Location = New System.Drawing.Point(145, 104)
+        Me.txtFn.Name = "txtFn"
+        Me.txtFn.Size = New System.Drawing.Size(100, 20)
+        Me.txtFn.TabIndex = 0
+        '
+        'txtSn
+        '
+        Me.txtSn.Location = New System.Drawing.Point(145, 179)
+        Me.txtSn.Name = "txtSn"
+        Me.txtSn.Size = New System.Drawing.Size(100, 20)
+        Me.txtSn.TabIndex = 0
+        '
+        'txtdob
+        '
+        Me.txtdob.Location = New System.Drawing.Point(145, 257)
+        Me.txtdob.Name = "txtdob"
+        Me.txtdob.Size = New System.Drawing.Size(100, 20)
+        Me.txtdob.TabIndex = 0
+        Me.txtdob.Text = " "
+        '
+        'txtadress
+        '
+        Me.txtadress.Location = New System.Drawing.Point(515, 103)
+        Me.txtadress.Name = "txtadress"
+        Me.txtadress.Size = New System.Drawing.Size(121, 20)
+        Me.txtadress.TabIndex = 0
+        '
+        'txtmobile
+        '
+        Me.txtmobile.Location = New System.Drawing.Point(515, 176)
+        Me.txtmobile.Name = "txtmobile"
+        Me.txtmobile.Size = New System.Drawing.Size(121, 20)
+        Me.txtmobile.TabIndex = 0
+        '
+        'txtemail
+        '
+        Me.txtemail.Location = New System.Drawing.Point(515, 255)
+        Me.txtemail.Name = "txtemail"
+        Me.txtemail.Size = New System.Drawing.Size(121, 20)
+        Me.txtemail.TabIndex = 0
+        '
+        'btndel
+        '
+        Me.btndel.Location = New System.Drawing.Point(91, 357)
+        Me.btndel.Name = "btndel"
+        Me.btndel.Size = New System.Drawing.Size(75, 23)
+        Me.btndel.TabIndex = 0
+        Me.btndel.Text = "Delete"
+        Me.btndel.UseVisualStyleBackColor = True
+        '
+        'btnext
+        '
+        Me.btnext.Location = New System.Drawing.Point(91, 416)
+        Me.btnext.Name = "btnext"
+        Me.btnext.Size = New System.Drawing.Size(75, 23)
+        Me.btnext.TabIndex = 0
+        Me.btnext.Text = "Exist"
+        Me.btnext.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FeildToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1370, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FeildToolStripMenuItem
+        '
+        Me.FeildToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.PrintToolStripMenuItem, Me.SaveToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FeildToolStripMenuItem.Name = "FeildToolStripMenuItem"
+        Me.FeildToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FeildToolStripMenuItem.Text = "File"
+        '
+        'PrintDocument1
+        '
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "student id"
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "firstname"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "surname"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "dob"
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "gender"
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "address"
+        Me.Column6.Name = "Column6"
+        Me.Column6.ReadOnly = True
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "mobile"
+        Me.Column7.Name = "Column7"
+        Me.Column7.ReadOnly = True
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "email"
+        Me.Column8.Name = "Column8"
+        Me.Column8.ReadOnly = True
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.NewToolStripMenuItem.Text = "new"
+        '
+        'PrintToolStripMenuItem
+        '
+        Me.PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
+        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PrintToolStripMenuItem.Text = "print"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SaveToolStripMenuItem.Text = "save"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "exit"
+        '
+        'cmbgen
+        '
+        Me.cmbgen.FormattingEnabled = True
+        Me.cmbgen.Items.AddRange(New Object() {"male", "female"})
+        Me.cmbgen.Location = New System.Drawing.Point(515, 42)
+        Me.cmbgen.Name = "cmbgen"
+        Me.cmbgen.Size = New System.Drawing.Size(121, 21)
+        Me.cmbgen.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1370, 761)
+        Me.Controls.Add(Me.cmbgen)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.txtemail)
+        Me.Controls.Add(Me.txtdob)
+        Me.Controls.Add(Me.txtmobile)
+        Me.Controls.Add(Me.txtSn)
+        Me.Controls.Add(Me.txtadress)
+        Me.Controls.Add(Me.txtFn)
+        Me.Controls.Add(Me.txtSid)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtSid As System.Windows.Forms.TextBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnreset As System.Windows.Forms.Button
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnPrint As System.Windows.Forms.Button
+    Friend WithEvents btnAddnew As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents txtFn As System.Windows.Forms.TextBox
+    Friend WithEvents txtSn As System.Windows.Forms.TextBox
+    Friend WithEvents txtdob As System.Windows.Forms.TextBox
+    Friend WithEvents txtadress As System.Windows.Forms.TextBox
+    Friend WithEvents txtmobile As System.Windows.Forms.TextBox
+    Friend WithEvents txtemail As System.Windows.Forms.TextBox
+    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents btnext As System.Windows.Forms.Button
+    Friend WithEvents btndel As System.Windows.Forms.Button
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FeildToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintDocument1 As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As System.Windows.Forms.PrintPreviewDialog
+    Friend WithEvents NewToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents cmbgen As System.Windows.Forms.ComboBox
+
+End Class
---
+++ VB_DataGridView_Saver/Form1.vb
@@ -0,0 +1,159 @@
+﻿Imports System.Drawing.Printing
+Imports System.Data.DataTable
+Imports excel = Microsoft.office.interop.Excel
+Imports Microsoft.office.interop
+Imports System.IO
+
+
+
+
+
+Public Class Form1
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnext_Click(sender As Object, e As EventArgs) Handles btnext.Click
+        MeExit()
+    End Sub
+    Private Sub MeExit()
+        Dim iExit As DialogResult
+        iExit = MessageBox.Show("confrim if you want to exsit", "Datagridview System", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+        If iExit = Windows.Forms.DialogResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        MeExit()
+    End Sub
+
+    Private Sub FeildToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FeildToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub btndel_Click(sender As Object, e As EventArgs) Handles btndel.Click
+        For Each row As DataGridViewRow In DataGridView1.SelectedRows
+            DataGridView1.Rows.Remove(row)
+
+        Next
+    End Sub
+
+    Private Sub btnreset_Click(sender As Object, e As EventArgs) Handles btnreset.Click
+        Dim txt
+        For Each txt In Me.Controls
+            If TypeOf txt Is TextBox Then
+                txt.text = ""
+            End If
+        Next txt
+    End Sub
+
+    Private Sub btnAddnew_Click(sender As Object, e As EventArgs) Handles btnAddnew.Click
+        DataGridView1.Rows.Add(txtSid.Text, txtFn.Text, txtSn.Text, txtdob.Text, txtadress.Text, txtemail.Text, cmbgen.Text, txtmobile.Text)
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        DataGridView1.Rows.Add(txtSid.Text, txtFn.Text, txtSn.Text, txtdob.Text, txtadress.Text, txtemail.Text, cmbgen.Text, txtmobile.Text)
+    End Sub
+    Private bitmap As Bitmap
+    Private Sub iPrint()
+        Dim height As Integer = DataGridView1.Height
+        DataGridView1.Height = DataGridView1.RowCount * DataGridView1.RowTemplate.Height
+        bitmap = New Bitmap(Me.DataGridView1.Width, Me.DataGridView1.Height)
+        DataGridView1.DrawToBitmap(bitmap, New Rectangle(0, 0, Me.DataGridView1.Width, Me.DataGridView1.Height))
+        PrintPreviewDialog1.Document = PrintDocument1
+        PrintPreviewDialog1.PrintPreviewControl.Zoom = 1
+        PrintPreviewDialog1.ShowDialog()
+        DataGridView1.Height = height
+    End Sub
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        iPrint()
+    End Sub
+
+    Private Sub PrintToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintToolStripMenuItem.Click
+        iPrint()
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        e.Graphics.DrawImage(bitmap, 0, 0)
+
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        SaveToExcel()
+
+    End Sub
+    Private Sub SaveToExcel()
+        Dim excel As Microsoft.Office.Interop.Excel.Application = New Microsoft.Office.Interop.Excel.Application()
+        Dim workbook As Microsoft.Office.Interop.Excel._Workbook = excel.Workbooks.Add(Type.Missing)
+        Dim worksheet As Microsoft.Office.Interop.Excel.Worksheet = Nothing
+        Try
+            worksheet = workbook.ActiveSheet
+
+            worksheet.Name = "ExportedFromDataGrid"
+
+            Dim cellrowindex As Integer = 1
+            Dim cellcolumindex As Integer = 1
+
+
+            For j As Integer = 0 To DataGridView1.Columns.Count - 1
+                worksheet.Cells(cellrowindex, cellcolumindex) = DataGridView1.Columns(j).HeaderText
+                cellcolumindex += 1
+
+            Next
+            cellcolumindex = 1
+            cellrowindex += 1
+
+            For i As Integer = 0 To DataGridView1.Rows.Count - 2
+
+            Next
+            For j As Integer = 0 To DataGridView1.Columns.Count - 1
+                worksheet.Cells(cellrowindex, cellcolumindex) = DataGridView1.Rows(i).Cells(j).Value.ToString()
+                cellcolumindex += 1
+            Next
+            cellcolumindex = 1
+            cellrowindex += 1
+
+            Dim saveDialog As New SaveFileDialog()
+            saveDialog.Filter = "Excel files (*.xlsx)|*.xlsx|All Files(*.*)|*.*"
+            saveDialog.FilterIndex = 2
+
+            If saveDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                workbook.SaveAs(saveDialog.FileName)
+                MessageBox.Show("export successful")
+
+            End If
+
+        Catch ex As System.Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            excel.Quit()
+            workbook = Nothing
+            excel = Nothing
+
+        End Try
+
+    End Sub
+
+    Private Function i() As Integer
+        Throw New NotImplementedException
+    End Function
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        SaveToExcel()
+
+    End Sub
+End Class
